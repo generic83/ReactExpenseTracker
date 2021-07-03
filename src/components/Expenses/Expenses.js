7cf1d768c25fb606c21699147ca12e6d0be65924
fadi lutfi <fadi-lutfi@hotmail.com>
2021-07-03 17:06:27 +0200

    Updates

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,38 @@
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import React, { useState } from 'react';
 
-function Expenses(props){
-    
+function Expenses(props) {
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+        console.log(selectedYear);
+        // Test two-way binding
+        // setTimeout(() => {
+        //     setFilteredYear('2021')
+        // }, 4000)
+    };
+
     return (
-        <Card className="expenses"> 
-         <ExpenseItem 
-         title={props.items[0].title} 
-         amount={props.items[0].amount} 
-         date={props.items[0].date}/>
-        </Card>
+        <div>
+            <Card className="expenses">
+                <ExpensesFilter  // controlled custom component - two-way binding
+                    selected={filteredYear}
+                    onChangeFilter={filterChangeHandler} />
+                {
+                    props.items.map(item =>
+                        <ExpenseItem
+                            title={item.title}
+                            amount={item.amount}
+                            date={item.date} />
+                    )
+                }
+
+            </Card>
+        </div>
     );
 }
 
